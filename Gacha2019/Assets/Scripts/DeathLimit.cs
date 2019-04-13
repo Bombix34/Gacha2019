14363a5cc00d93b98ba62e0bd627f8703a6f4db0
Bomb-X <bombix34@gmail.com>
2019-04-14 00:23:30 +0200

    Plan qui tue le joueur + game over placeholder (pas diégétique du tout)

---
+++ Gacha2019/Assets/Scripts/DeathLimit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathLimit : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameManager.instance.GameOver(collision.gameObject);
+        }
+    }
+}
---
+++ Gacha2019/Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    bool isGameOver=false;
+
+    public GameObject gameoverPlaceholder;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(isGameOver)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                Restart();
+            }
+        }
+    }
+
+
+    public void GameOver(GameObject player)
+    {
+        isGameOver = true;
+        player.GetComponent<Player>().enabled = false;
+        Instantiate(gameoverPlaceholder, transform.position, Quaternion.identity);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+
+    //SINGLETON________________________________________________________________________________________________
+    private static GameManager s_Instance = null;
+
+    // This defines a static instance property that attempts to find the manager object in the scene and
+    // returns it to the caller.
+    public static GameManager instance
+    {
+        get
+        {
+            if (s_Instance == null)
+            {
+                // This is where the magic happens.
+                //  FindObjectOfType(...) returns the first AManager object in the scene.
+                s_Instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+            }
+
+            // If it is still null, create a new instance
+            if (s_Instance == null)
+            {
+                Debug.Log("error");
+                GameObject obj = new GameObject("Error");
+                s_Instance = obj.AddComponent(typeof(GameManager)) as GameManager;
+            }
+
+            return s_Instance;
+        }
+    }
+}
