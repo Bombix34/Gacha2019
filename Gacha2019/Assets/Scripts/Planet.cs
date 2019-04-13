7865a59814e9890b297596f68db46d2b3bec63b8
Bomb-X <bombix34@gmail.com>
2019-04-13 22:53:04 +0200

    Planète qui rétrecie version 1

--- Gacha2019/Assets/Scripts/Planet.cs
+++ Gacha2019/Assets/Scripts/Planet.cs
@@ -37,9 +37,14 @@ public class Planet : MonoBehaviour
     [SerializeField]
     private GameObject m_ShooterPrefab = null;
 
+    [SerializeField]
+    private GameObject m_TriggerEndPrefab = null;
+
     private Vector3 m_LastMousePosition;
 
     private bool m_IsCursorPressed = false;
+
+    private List<GameObject> m_objectsOnPlanet;
 
     public float Radius
     {
@@ -57,42 +62,15 @@ public class Planet : MonoBehaviour
         }
     }
 
+    private void Awake()
+    {
+        m_objectsOnPlanet = new List<GameObject>();
+    }
+
     private void Start()
     {
-        if (m_PlanetModel != null)
-        {
-            m_PlanetModel.localScale = new Vector3(m_Radius * 2, m_Radius * 2, m_Radius * 2);
-        }
-        if (m_ObstaclePrefab != null)
-        {
-            for (int i = 0; i < m_ObstacleCount; i++)
-            {
-                Vector3 localPosition = new Vector3(0, Radius, 0);
-                Quaternion rotation = Quaternion.Euler(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), 0);
-                localPosition = rotation * localPosition;
-                Instantiate(m_ObstaclePrefab, transform.position + localPosition, rotation, m_PlanetAutoRotation);
-            }
-        }
-        if (m_DestructiblePrefab != null)
-        {
-            for (int i = 0; i < m_DestructibleCount; i++)
-            {
-                Vector3 localPosition = new Vector3(0, Radius, 0);
-                Quaternion rotation = Quaternion.Euler(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), 0);
-                localPosition = rotation * localPosition;
-                Instantiate(m_DestructiblePrefab, transform.position + localPosition, rotation, m_PlanetAutoRotation);
-            }
-        }
-        if (m_ShooterPrefab != null)
-        {
-            for (int i = 0; i < m_ShooterCount; i++)
-            {
-                Vector3 localPosition = new Vector3(0, Radius, 0);
-                Quaternion rotation = Quaternion.Euler(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), 0);
-                localPosition = rotation * localPosition;
-                Instantiate(m_ShooterPrefab, transform.position + localPosition, rotation, m_PlanetAutoRotation);
-            }
-        }
+        ScalePlanet();
+        InitObjectOnPlanet();
     }
 
     private void Update()
@@ -121,4 +99,99 @@ public class Planet : MonoBehaviour
         }
         transform.Rotate(-m_RotationSpeed * Time.deltaTime, 0, 0);
     }
+
+    private void InitObjectOnPlanet()
+    {
+        //PLACEMENT ALEATOIRE DES OBJETS
+        if (m_ObstaclePrefab != null)
+        {
+            for (int i = 0; i < m_ObstacleCount; i++)
+            {
+                Vector3 localPosition = new Vector3(0, Radius, 0);
+                Quaternion rotation = Quaternion.Euler(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), 0);
+                localPosition = rotation * localPosition;
+                m_objectsOnPlanet.Add(Instantiate(m_ObstaclePrefab, transform.position + localPosition, rotation, m_PlanetAutoRotation));
+            }
+        }
+        if (m_DestructiblePrefab != null)
+        {
+            for (int i = 0; i < m_DestructibleCount; i++)
+            {
+                Vector3 localPosition = new Vector3(0, Radius, 0);
+                Quaternion rotation = Quaternion.Euler(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), 0);
+                localPosition = rotation * localPosition;
+                m_objectsOnPlanet.Add(Instantiate(m_DestructiblePrefab, transform.position + localPosition, rotation, m_PlanetAutoRotation));
+            }
+        }
+        if (m_ShooterPrefab != null)
+        {
+            for (int i = 0; i < m_ShooterCount; i++)
+            {
+                Vector3 localPosition = new Vector3(0, Radius, 0);
+                Quaternion rotation = Quaternion.Euler(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), 0);
+                localPosition = rotation * localPosition;
+                m_objectsOnPlanet.Add(Instantiate(m_ShooterPrefab, transform.position + localPosition, rotation, m_PlanetAutoRotation));
+            }
+        }
+        if(m_TriggerEndPrefab != null)
+        {
+            Vector3 localPosition = new Vector3(0, Radius, 0);
+            Quaternion rotation = Quaternion.Euler(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), 0);
+            localPosition = rotation * localPosition;
+            m_objectsOnPlanet.Add(Instantiate(m_TriggerEndPrefab, transform.position + localPosition, rotation, m_PlanetAutoRotation));
+        }
+    }
+
+    public void ResetObjectsOnPlanet()
+    {
+        foreach(GameObject obj in m_objectsOnPlanet)
+        {
+            Destroy(obj);
+        }
+    }
+
+    private void ScalePlanet()
+    {
+        if (m_PlanetModel != null)
+        {
+            m_PlanetModel.localScale = new Vector3(m_Radius * 2, m_Radius * 2, m_Radius * 2);
+        }
+    }
+
+    public void SetUpNextPlanet()
+    {
+        m_Radius *= 0.8f;
+        ResetObjectsOnPlanet();
+        ScalePlanet();
+        InitObjectOnPlanet();
+    }
+
+    //SINGLETON________________________________________________________________________________________________
+    private static Planet s_Instance = null;
+
+    // This defines a static instance property that attempts to find the manager object in the scene and
+    // returns it to the caller.
+    public static Planet instance
+    {
+        get
+        {
+            if (s_Instance == null)
+            {
+                // This is where the magic happens.
+                //  FindObjectOfType(...) returns the first AManager object in the scene.
+                s_Instance = FindObjectOfType(typeof(Planet)) as Planet;
+            }
+
+            // If it is still null, create a new instance
+            if (s_Instance == null)
+            {
+                Debug.Log("error");
+                GameObject obj = new GameObject("Error");
+                s_Instance = obj.AddComponent(typeof(Planet)) as Planet;
+            }
+
+            return s_Instance;
+        }
+    }
+
 }
