59875a490fc6071c3ee2dd1afb03d22223395329
Kiok <gonzalez.damien.kiok@gmail.com>
2019-04-14 19:05:39 +0200

    Paint Mode Sur Level Editor

---
+++ Gacha2019/Assets/Scripts/EditorCurve.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EditorCurve
+{
+    public Vector3 pinStart;
+    public Vector3 pinEnd;
+
+    public Vector3 pinStartDirection;
+
+
+    public Vector3 pinEndDirection;
+
+    public float Step;
+
+    public bool Complete;
+
+    public EditorCurve(Vector3 start, Vector3 dir)
+    {
+        pinStart = start;
+        pinStartDirection = dir;
+
+    }
+    
+    public void SetEnd(Vector3 end, Vector3 dir)
+    {
+        pinEnd = end;
+        pinEndDirection = dir;
+        Complete = true;
+    }
+
+    public Vector3[] positionsSteped()
+    {
+
+        Vector3 mover = pinStart;
+
+        int Size = Mathf.RoundToInt(Vector3.Distance(pinStart, pinEnd) / Step);
+
+        Vector3[] positionsAlong = new Vector3[Size];
+
+        
+
+        for (int i = 0; i < Size; i++)
+        {
+            positionsAlong[i] = mover;
+
+            mover = Vector3.MoveTowards(mover, pinEnd, Step);
+
+
+
+        }
+
+        return positionsAlong;
+
+    }
+
+
+}
--- Gacha2019/Assets/Scripts/PlanetEditor.cs
+++ Gacha2019/Assets/Scripts/PlanetEditor.cs
@@ -13,17 +13,30 @@ public class PlanetEditor : MonoBehaviour
     public GameObject prefab;
 
 
+    
+
     [HideInInspector]
     public Transform m_Parent;
     [HideInInspector]
     public bool EditModeActive;
+    //[HideInInspector]
+    public bool PaintModeActive;
     [HideInInspector]
     public bool RotationActivated;
 
     public float speed;
-
+    [HideInInspector]
+    public bool MakeLine;
 
     Vector2 mouseDelta;
+
+    Vector2 lineStart;
+
+    Vector2 lineEnd;
+
+    public float Step;
+
+    EditorCurve Curve;
 
     private void OnEnable()
     {
@@ -85,14 +98,31 @@ public class PlanetEditor : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit))
             {
-                //GameObject go = Instantiate(prefab, m_Parent);
 
-                GameObject go = (GameObject)PrefabUtility.InstantiatePrefab(prefab, m_Parent);
-                Undo.RegisterCreatedObjectUndo(go, "Created go");
+                if(!PaintModeActive)
+                {
+                    //GameObject go = Instantiate(prefab, m_Parent);
 
-                go.transform.localScale = Vector3.one;
-                go.transform.position = hit.point;
-                go.transform.up = go.transform.position.normalized;
+
+                    Debug.Log("INSTANCIATE");
+                    GameObject go = (GameObject)PrefabUtility.InstantiatePrefab(prefab, m_Parent);
+                    Undo.RegisterCreatedObjectUndo(go, "Created go");
+
+                    go.transform.localScale = Vector3.one;
+                    go.transform.position = hit.point;
+                    go.transform.up = go.transform.position.normalized;
+                }
+                else
+                {
+                    MakeLine = true;
+                    lineStart = e.mousePosition;
+
+                    Curve = new EditorCurve(hit.point, hit.normal);
+
+                    //gameObject.AddComponent<BezierSpline>().AddCurve();
+                    
+                }
+               
             }
             else
             {
@@ -106,41 +136,105 @@ public class PlanetEditor : MonoBehaviour
         {
             RotationActivated = false;
 
-        }
-
-        if (e.type == EventType.MouseDown && e.button == 1)
-        {
-
-
-            Vector3 mousePos = e.mousePosition;
-            float ppp = EditorGUIUtility.pixelsPerPoint;
-            mousePos.y = scene.camera.pixelHeight - mousePos.y * ppp;
-            mousePos.x *= ppp;
-
-
-
-            RaycastHit hit;
-            Ray ray = scene.camera.ScreenPointToRay(mousePos);
-
-            if (Physics.Raycast(ray, out hit))
+            if (PaintModeActive)
             {
-                Debug.Log("HIT" + hit.transform.name);
+                Vector3 mousePos = e.mousePosition;
+                float ppp = EditorGUIUtility.pixelsPerPoint;
+                mousePos.y = scene.camera.pixelHeight - mousePos.y * ppp;
+                mousePos.x *= ppp;
 
-                //GameObject go = Instantiate(prefab, m_Parent);
-                /*
-                GameObject go = (GameObject)PrefabUtility.InstantiatePrefab(prefab, m_Parent);
-                Undo.RegisterCreatedObjectUndo(go, "Created go");
 
-                go.transform.localScale = Vector3.one;
-                go.transform.position = hit.point;
-                go.transform.up = go.transform.position.normalized;*/
+
+                RaycastHit hit;
+                Ray ray = scene.camera.ScreenPointToRay(mousePos);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Curve.SetEnd(hit.point, hit.normal);
+                    Curve.Step = Step;
+                    InstanciateLine(Curve.positionsSteped());
+                    Curve = null;
+                }
+                else
+                    Curve = null;
+
+                //DestroyImmediate(gameObject.GetComponent<BezierSpline>());
+                MakeLine = false;
             }
-           
 
-            e.Use();
         }
+
+        
 
     }
+
+    private void OnDrawGizmos()
+    {
+        if(Curve != null)
+        {
+            if(Curve.pinStart != null)
+            Gizmos.DrawSphere(Curve.pinStart, 0.5f);
+                if(Curve.pinEnd != null)
+            Gizmos.DrawSphere(Curve.pinEnd, 0.5f);
+
+            if (Curve.Complete)
+            {
+                Handles.DrawLine(Curve.pinStart, Curve.pinEnd);
+
+                Gizmos.color = Color.red;
+
+                foreach (Vector3 v in Curve.positionsSteped())
+                {
+                    Gizmos.DrawSphere(v, 0.5f);
+                }
+            }
+               
+
+        }
+       
+
+
+
+
+
+    }
+
+    public void InstanciateLine(Vector3[] linePositions)
+    {
+        for (int i = 0; i < linePositions.Length; i++)
+        {
+            GameObject go = (GameObject)PrefabUtility.InstantiatePrefab(prefab, m_Parent);
+            Undo.RegisterCreatedObjectUndo(go, "Created go");
+
+            RaycastHit hit;
+
+            if (Physics.Linecast(linePositions[i] * 2, linePositions[i], out hit))
+            {
+                go.transform.position = hit.point;
+            }
+            else
+                go.transform.position = linePositions[i];
+
+
+
+            go.transform.localScale = Vector3.one;
+            
+            go.transform.up = go.transform.position.normalized;
+
+            //go.transform.LookAt(linePositions[0]);
+
+
+
+        }
+
+
+    }
+
+    
+
+
+   
+
 }
 
 #endif
