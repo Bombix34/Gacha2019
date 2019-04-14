bd8276e44b5a6cc19c22bd3aad2c0d1fac0f63fc
Bomb-X <bombix34@gmail.com>
2019-04-14 11:22:03 +0200

    Particules Player + rotative skybox

---
+++ Gacha2019/Assets/Scripts/SkyboxManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxManager : MonoBehaviour
+{
+
+    public float SkyboxSpeed = 1f;
+
+    void Update()
+    {
+        GetComponent<Skybox>().material.SetFloat("_Rotation", Time.time* SkyboxSpeed);
+    }
+
+}
