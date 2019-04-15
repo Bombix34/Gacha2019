00e0a497b0acac95260ec5a0630dc62f48cb8ebe
Lucas Linares <lucaslinares59@gmail.com>
2019-04-15 14:15:55 +0200

    main menu finished

---
+++ Gacha2019/Assets/Scripts/UI/WaitForMainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitForMainMenu : MonoBehaviour
+{
+
+    public MainMenuManager manager = null;
+    public float timer = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0f)
+        {
+            manager.ChangeToMainMenu(gameObject);
+        }
+    }
+}
